f3ae57ea9002af6e4f85033c9530f65edb646eaf
Anand <anandtrex@users.noreply.github.com>
2018-04-23 18:09:04 +0200

    Support for PPC

--- setup.py
+++ setup.py
@@ -24,8 +24,9 @@ PY3 = sys.version_info[0] == 3
 
 # determine CPU support for SSE2 and AVX2
 cpu_info = cpuinfo.get_cpu_info()
-have_sse2 = 'sse2' in cpu_info['flags']
-have_avx2 = 'avx2' in cpu_info['flags']
+flags = cpu_info.get('flags', [])
+have_sse2 = 'sse2' in flags
+have_avx2 = 'avx2' in flags
 disable_sse2 = 'DISABLE_NUMCODECS_SSE2' in os.environ
 disable_avx2 = 'DISABLE_NUMCODECS_AVX2' in os.environ
 
