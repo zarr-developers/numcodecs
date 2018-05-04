88bf001f66344b15bc34b2c51da852338951d24d
Jerome Kelleher <jk@well.ox.ac.uk>
2018-05-04 11:32:14 +0100

    Ensures that get_codec doesn't modify its argument.
    Closes #78.

--- numcodecs/tests/test_registry.py
+++ numcodecs/tests/test_registry.py
@@ -11,3 +11,11 @@ from numcodecs.registry import get_codec
 def test_registry_errors():
     with pytest.raises(ValueError):
         get_codec({'id': 'foo'})
+
+
+def test_get_codec_argument():
+    # Check that get_codec doesn't modify its argument.
+    arg = {"id": "json"}
+    before = dict(arg)
+    get_codec(arg)
+    assert before == arg
