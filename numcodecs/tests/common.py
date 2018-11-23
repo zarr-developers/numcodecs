cf386f9929a5f193d29fb628f11af5ded3b6e856
Alistair Miles <alimanfoo@googlemail.com>
2018-11-23 12:07:30 +0000

    Revert "simplify" - need to allow for object arrays in backwards compatibility tests
    This reverts commit 047ac0a95846ecdd1e2b98a55abc7954fcfabf78.

--- numcodecs/tests/common.py
+++ numcodecs/tests/common.py
@@ -225,7 +225,10 @@ def check_backwards_compatibility(codec_id, arrays, codecs, precision=None, pref
             with open(enc_fn, mode='rb') as ef:
                 enc = ef.read()
                 dec = codec.decode(enc)
-                dec_arr = ensure_contiguous_ndarray(dec)
+                if isinstance(dec, np.ndarray):
+                    dec_arr = dec
+                else:
+                    dec_arr = ensure_contiguous_ndarray(dec)
                 dec_arr = dec_arr.view(dtype=arr.dtype).reshape(arr.shape, order=order)
                 if precision and precision[j] is not None:
                     assert_array_almost_equal(arr, dec_arr, decimal=precision[j])
