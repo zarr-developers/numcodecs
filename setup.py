ebd891b02593a8bbbe3da234719d74a6cbf3a18b
Alistair Miles <alimanfoo@googlemail.com>
2018-11-23 10:14:17 +0000

    use pyproject.toml to specify build requirements

--- setup.py
+++ setup.py
@@ -313,10 +313,6 @@ def run_setup(with_extensions):
             'local_scheme': 'dirty-tag',
             'write_to': 'numcodecs/version.py'
         },
-        setup_requires=[
-            'setuptools>18.0',
-            'setuptools-scm>1.5.4'
-        ],
         install_requires=[
             'numpy>=1.7',
         ],
