5033145a816e8db56a10ba9050afb478d67527f6
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-11-28 11:05:58 -0500

    Test for poorly sized `out` with `GZip`
    Make sure we raise a `ValueError` if `out` is too small to fit the data
    or larger than the data.

--- numcodecs/tests/test_gzip.py
+++ numcodecs/tests/test_gzip.py
@@ -93,3 +93,22 @@ def test_err_encode_non_contiguous():
     for codec in codecs:
         with pytest.raises(ValueError):
             codec.encode(arr)
+
+
+def test_err_out_too_small():
+    # non-contiguous memory
+    arr = np.arange(10, dtype='i4')
+    out = np.empty_like(arr)[:-1]
+    for codec in codecs:
+        with pytest.raises(ValueError):
+            codec.decode(codec.encode(arr), out)
+
+
+def test_err_out_too_large():
+    # non-contiguous memory
+    out = np.empty((10,), dtype='i4')
+    arr = out[:-1]
+    arr[:] = 5
+    for codec in codecs:
+        with pytest.raises(ValueError):
+            codec.decode(codec.encode(arr), out)
