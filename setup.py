06d40699fb927d2c5e64055c6386df325a16bd97
halehawk <haiyingx@ucar.edu>
2019-01-11 14:45:12 -0700

    disable openmp and disable cfp compilation

--- setup.py
+++ setup.py
@@ -219,6 +219,8 @@ def zfp_extension():
     include_dirs += ['numcodecs']
     include_dirs += [numpy.get_include()]
     # define_macros += [('CYTHON_TRACE', '1')]
+    extra_compile_args.append('-DZFP_WITH_OPENMP=OFF')
+    extra_compile_args.append('-DBUILD_CFP=OFF')
     extra_compile_args += [
        '-std=c99', 
     ]
