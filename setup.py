7c07db404ecdf780cc0ac37e0b2d04660ef852fc
jakirkham <jakirkham@gmail.com>
2019-11-08 21:53:31 -0500

    Force `libc++` on macOS (#211)
    * Force `libc++` on macOS
    
    Some Python builds still try to use `stdlibc++`, which really doesn't
    exist on macOS any more. So force `libc++` to bypass this issue.
    
    * Refine comment about libc++ flag

--- setup.py
+++ setup.py
@@ -51,6 +51,10 @@ elif os.name == 'posix':
 # workaround lack of support for "inline" in MSVC when building for Python 2.7 64-bit
 if PY2 and os.name == 'nt':
     base_compile_args.append('-Dinline=__inline')
+# On macOS, force libc++ in case the system tries to use `stdlibc++`.
+# The latter is often absent from modern macOS systems.
+if sys.platform == 'darwin':
+    base_compile_args.append('-stdlib=libc++')
 
 
 def info(*msg):
