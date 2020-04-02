5106af146343c713e7babbb01418f028b5035173
James Bourbeau <jrbourbeau@gmail.com>
2020-04-02 13:39:08 -0500

    Remove LegacyJSON codec

--- numcodecs/__init__.py
+++ numcodecs/__init__.py
@@ -99,9 +99,8 @@ from numcodecs.checksum32 import CRC32, Adler32
 register_codec(CRC32)
 register_codec(Adler32)
 
-from numcodecs.json import JSON, LegacyJSON
+from numcodecs.json import JSON
 register_codec(JSON)
-register_codec(LegacyJSON)
 
 try:
     from numcodecs import vlen
