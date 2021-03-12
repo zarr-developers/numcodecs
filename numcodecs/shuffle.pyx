48508259a6e1b0c081b904201257f2f4bb2f7ec8
Paul Branson <pbranson@gmail.com>
2021-03-12 11:41:15 +0800

    Update numcodecs/shuffle.pyx
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- numcodecs/shuffle.pyx
+++ numcodecs/shuffle.pyx
@@ -12,7 +12,7 @@ cimport cython
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef void _doShuffle(const unsigned char[::1] src, unsigned char[::1] des, int element_size) nogil:
+cdef void _doShuffle(const unsigned char[::1] src, unsigned char[::1] des, Py_ssize_t element_size) nogil:
     cdef Py_ssize_t count, i, j, offset, byte_index
     count = len(src) // element_size
     for i in range(count):
