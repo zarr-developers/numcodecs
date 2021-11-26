1a60ad30b6e45dbe43d24366f3bbb90950f422e9
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-11-26 12:49:08 +0100

    Unnecessary `None` provided to `get()`

--- numcodecs/registry.py
+++ numcodecs/registry.py
@@ -28,7 +28,7 @@ def get_codec(config):
     """
     config = dict(config)
     codec_id = config.pop('id', None)
-    cls = codec_registry.get(codec_id, None)
+    cls = codec_registry.get(codec_id)
     if cls is None:
         raise ValueError('codec not available: %r' % codec_id)
     return cls.from_config(config)
