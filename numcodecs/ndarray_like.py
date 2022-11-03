815d964ff2eeab894436ddf8b8c6624d66962424
Andreas Poehlmann <andreas@poehlmann.io>
2022-11-03 10:13:45 +0100

    numcodecs.ndarray_like: cache results of isinstance checks

--- numcodecs/ndarray_like.py
+++ numcodecs/ndarray_like.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Any, Optional, Tuple
+from typing import Any, Dict, Optional, Tuple, Type
 
 if sys.version_info >= (3, 8):
     from typing import Protocol, runtime_checkable
@@ -7,22 +7,41 @@ else:
     from typing_extensions import Protocol, runtime_checkable
 
 
+class _CachedProtocolMeta(Protocol.__class__):
+    """Custom implementation of @runtime_checkable
+
+    The native implementation of @runtime_checkable is slow,
+    see <https://github.com/zarr-developers/numcodecs/issues/379>.
+
+    This metaclass keeps an unbounded cache of the result of
+    isinstance checks using the object's class as the cache key.
+    """
+    _instancecheck_cache: Dict[Type, bool] = {}
+
+    def __instancecheck__(cls, instance):
+        ret = cls._instancecheck_cache.get(instance.__class__, None)
+        if ret is None:
+            ret = super().__instancecheck__(instance)
+            cls._instancecheck_cache[instance.__class__] = ret
+        return ret
+
+
 @runtime_checkable
-class DType(Protocol):
+class DType(Protocol, metaclass=_CachedProtocolMeta):
     itemsize: int
     name: str
     kind: str
 
 
 @runtime_checkable
-class FlagsObj(Protocol):
+class FlagsObj(Protocol, metaclass=_CachedProtocolMeta):
     c_contiguous: bool
     f_contiguous: bool
     owndata: bool
 
 
 @runtime_checkable
-class NDArrayLike(Protocol):
+class NDArrayLike(Protocol, metaclass=_CachedProtocolMeta):
     dtype: DType
     shape: Tuple[int, ...]
     strides: Tuple[int, ...]
@@ -51,26 +70,6 @@ class NDArrayLike(Protocol):
         ...
 
 
-# keep in sync with NDArrayLike
-_NDArrayLike_protocol_attrs = [
-    "dtype",
-    "shape",
-    "strides",
-    "ndim",
-    "size",
-    "itemsize",
-    "nbytes",
-    "flags",
-    "__len__",
-    "__getitem__",
-    "__setitem__",
-    "tobytes",
-    "reshape",
-    "view",
-]
-
-
 def is_ndarray_like(obj: object) -> bool:
     """Return True when `obj` is ndarray-like"""
-    # same as `isinstance(obj, NDArrayLike)` but faster
-    return all(hasattr(obj, attr) for attr in _NDArrayLike_protocol_attrs)
+    return isinstance(obj, NDArrayLike)
