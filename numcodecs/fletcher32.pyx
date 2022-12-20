8e01f63a9929de561f23c11365b54813c583bc9a
Martin Durant <martin.durant@alumni.utoronto.ca>
2022-12-20 15:21:01 -0500

    Remove unused, add docstrings

--- numcodecs/fletcher32.pyx
+++ numcodecs/fletcher32.pyx
@@ -1,10 +1,4 @@
-# cython: boundscheck=False
-# cython: wraparound=False
-# cython: overflowcheck=False
-# cython: cdivision=True
-
 import struct
-import numpy as np
 
 from numcodecs.abc import Codec
 from numcodecs.compat import ensure_contiguous_ndarray
@@ -13,6 +7,7 @@ from libc.stdint cimport uint8_t, uint16_t, uint32_t
 
 cdef extern from "_fletcher.c":
     uint32_t H5_checksum_fletcher32(const void *_data, size_t _len)
+
 
 class Fletcher32(Codec):
     """The fletcher checksum with 16-bit words and 32-bit output
@@ -26,12 +21,14 @@ class Fletcher32(Codec):
     codec_id = "fletcher32"
 
     def encode(self, buf):
+        """Return buffer plus 4-byte fletcher checksum"""
         buf = ensure_contiguous_ndarray(buf).ravel().view('uint8')
         cdef const uint8_t[::1] b_ptr = buf
         val = H5_checksum_fletcher32(&b_ptr[0], buf.nbytes)
         return buf.tobytes() + struct.pack("<I", val)
 
     def decode(self, buf, out=None):
+        """Check fletcher checksum, and return buffer without it"""
         b = ensure_contiguous_ndarray(buf).view('uint8')
         cdef const uint8_t[::1] b_ptr = b
         val = H5_checksum_fletcher32(&b_ptr[0], b.nbytes - 4)
