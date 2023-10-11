b52d1c0c3ba1787c1420b4fe7a0af8bf81f5dc45
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-10 18:11:10 -0700

    Change test to fork

--- numcodecs/tests/test_entrypoints.py
+++ numcodecs/tests/test_entrypoints.py
@@ -4,6 +4,8 @@ from unittest import mock
 
 import pytest
 
+from multiprocessing import Process
+
 import numcodecs.registry
 
 
@@ -30,16 +32,19 @@ def test_entrypoint_codec():
     assert cls.codec_id == "test"
 
 
-def test_entrypoint_codec_with_importlib_metadata():
+def get_entrypoints_with_importlib_metadata_loaded():
     # importlib_metadata patches importlib.metadata, which can lead to breaking changes
     # to the APIs of EntryPoint objects used when registering entrypoints. Attempt to
     # isolate those changes to just this test.
-    with mock.patch.dict(sys.modules):
-        import importlib_metadata  # noqa: F401
-        sys.path.append(here)
-        numcodecs.registry.run_entrypoints()
-        cls = numcodecs.registry.get_codec({"id": "test"})
-        assert cls.codec_id == "test"
-        sys.path.remove(here)
-        numcodecs.registry.run_entrypoints()
-        numcodecs.registry.codec_registry.pop("test")
+    import importlib_metadata  # noqa: F401
+    sys.path.append(here)
+    numcodecs.registry.run_entrypoints()
+    cls = numcodecs.registry.get_codec({"id": "test"})
+    assert cls.codec_id == "test"
+
+
+def test_entrypoint_codec_with_importlib_metadata():
+    p = Process(target=get_entrypoints_with_importlib_metadata_loaded)
+    p.start()
+    p.join()
+    assert p.exitcode == 0
