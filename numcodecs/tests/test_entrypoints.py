a5951d4ab838f2e39759e5a8218fbb3c461002c4
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-17 14:44:09 -0700

    Clean up old test

--- numcodecs/tests/test_entrypoints.py
+++ numcodecs/tests/test_entrypoints.py
@@ -9,6 +9,7 @@ import numcodecs.registry
 here = os.path.abspath(os.path.dirname(__file__))
 
 
+@pytest.fixture()
 def set_path():
     sys.path.append(here)
     numcodecs.registry.run_entrypoints()
@@ -18,12 +19,7 @@ def set_path():
     numcodecs.registry.codec_registry.pop("test")
 
 
-@pytest.fixture()
-def set_path_fixture():
-    yield from set_path()
-
-
-@pytest.mark.usefixtures("set_path_fixture")
+@pytest.mark.usefixtures("set_path")
 def test_entrypoint_codec():
     cls = numcodecs.registry.get_codec({"id": "test"})
     assert cls.codec_id == "test"
