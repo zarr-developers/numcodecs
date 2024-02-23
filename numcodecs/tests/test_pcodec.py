d6377737e7637935a664056e0479c9ceeeb58b05
Ryan Abernathey <ryan.abernathey@gmail.com>
2024-02-23 16:24:55 -0500

    skip pcodec tests if not installed

--- numcodecs/tests/test_pcodec.py
+++ numcodecs/tests/test_pcodec.py
@@ -1,8 +1,16 @@
 import pytest
 import numpy as np
 
-
 from numcodecs.pcodec import PCodec
+
+try:
+    # initializing codec triggers ImportError
+    PCodec()
+except ImportError:  # pragma: no cover
+    pytest.skip(
+        "pcodec not available", allow_module_level=True
+    )
+
 from numcodecs.tests.common import (
     check_encode_decode_array_to_bytes,
     check_config,
