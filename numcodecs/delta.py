c283106bd970952e6581a270e9bbad478e908860
jakirkham <jakirkham@gmail.com>
2024-10-12 03:16:21 -0700

    Update optimized `Delta` codec for `bool` (#595)
    * Update optimized `Delta` codec for `bool`
    
    Previously `bool` just worked with `Delta`. However this was not
    actually tested. The optimized version switched to `np.subtract` for
    in-place computation, which works for other types. Though `bool` needs
    special handling. Fortunately this can be done with `np.not_equal`,
    which has the same behavior.
    
    Also include a test for `bool` data to make sure this is handled
    correctly going forward.
    
    * Simplify type check
    
    This is a bit more succinct and gets to the core point. Namely
    `arr.dtype` determines this code path.
    
    Also comparing directly to `bool` works here. It is a bit faster as well
    since we need not construct an `np.dtype` object.
    
    * Drop extraneous `0`s

--- numcodecs/delta.py
+++ numcodecs/delta.py
@@ -64,7 +64,10 @@ class Delta(Codec):
 
         # compute differences
         # using np.subtract for in-place operations
-        np.subtract(arr[1:], arr[0:-1], out=enc[1:])
+        if arr.dtype == bool:
+            np.not_equal(arr[1:], arr[:-1], out=enc[1:])
+        else:
+            np.subtract(arr[1:], arr[:-1], out=enc[1:])
 
         return enc
 
