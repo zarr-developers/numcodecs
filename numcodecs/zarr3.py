a4cf7ad740448f906f9bc75499aae5e7a4d1cdd5
Matthew Iannucci <mpiannucci@gmail.com>
2024-10-16 08:52:42 -0400

    Sync with zarr 3 beta (#597)
    * Sync with zarr 3 beta
    
    * Update zarr version in ci
    
    * dont install zarr python 3 in workflows running 3.10

--- numcodecs/zarr3.py
+++ numcodecs/zarr3.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import asyncio
 from dataclasses import dataclass, replace
 from functools import cached_property
 import math
@@ -17,15 +18,15 @@ except ImportError:
     raise ImportError("zarr 3.0.0 or later is required to use the numcodecs zarr integration.")
 
 from zarr.abc.codec import ArrayArrayCodec, BytesBytesCodec, ArrayBytesCodec
-from zarr.buffer import NDBuffer, Buffer, BufferPrototype, as_numpy_array_wrapper
-from zarr.array_spec import ArraySpec
-from zarr.common import (
+from zarr.core.buffer import NDBuffer, Buffer, BufferPrototype
+from zarr.core.buffer.cpu import as_numpy_array_wrapper
+from zarr.core.array_spec import ArraySpec
+from zarr.core.common import (
     JSON,
     parse_named_configuration,
     product,
-    to_thread,
 )
-from zarr.metadata import ArrayMetadata
+from zarr.core.metadata import ArrayMetadata
 
 
 CODEC_PREFIX = "numcodecs."
@@ -90,7 +91,7 @@ class NumcodecsBytesBytesCodec(NumcodecsCodec, BytesBytesCodec):
         super().__init__(codec_id=codec_id, codec_config=codec_config)
 
     async def _decode_single(self, chunk_bytes: Buffer, chunk_spec: ArraySpec) -> Buffer:
-        return await to_thread(
+        return await asyncio.to_thread(
             as_numpy_array_wrapper,
             self._codec.decode,
             chunk_bytes,
@@ -104,7 +105,7 @@ class NumcodecsBytesBytesCodec(NumcodecsCodec, BytesBytesCodec):
         return prototype.buffer.from_bytes(encoded)
 
     async def _encode_single(self, chunk_bytes: Buffer, chunk_spec: ArraySpec) -> Buffer:
-        return await to_thread(self._encode, chunk_bytes, chunk_spec.prototype)
+        return await asyncio.to_thread(self._encode, chunk_bytes, chunk_spec.prototype)
 
 
 class NumcodecsArrayArrayCodec(NumcodecsCodec, ArrayArrayCodec):
@@ -113,12 +114,12 @@ class NumcodecsArrayArrayCodec(NumcodecsCodec, ArrayArrayCodec):
 
     async def _decode_single(self, chunk_array: NDBuffer, chunk_spec: ArraySpec) -> NDBuffer:
         chunk_ndarray = chunk_array.as_ndarray_like()
-        out = await to_thread(self._codec.decode, chunk_ndarray)
+        out = await asyncio.to_thread(self._codec.decode, chunk_ndarray)
         return chunk_spec.prototype.nd_buffer.from_ndarray_like(out.reshape(chunk_spec.shape))
 
     async def _encode_single(self, chunk_array: NDBuffer, chunk_spec: ArraySpec) -> NDBuffer:
         chunk_ndarray = chunk_array.as_ndarray_like()
-        out = await to_thread(self._codec.encode, chunk_ndarray)
+        out = await asyncio.to_thread(self._codec.encode, chunk_ndarray)
         return chunk_spec.prototype.nd_buffer.from_ndarray_like(out)
 
 
@@ -128,12 +129,12 @@ class NumcodecsArrayBytesCodec(NumcodecsCodec, ArrayBytesCodec):
 
     async def _decode_single(self, chunk_buffer: Buffer, chunk_spec: ArraySpec) -> NDBuffer:
         chunk_bytes = chunk_buffer.to_bytes()
-        out = await to_thread(self._codec.decode, chunk_bytes)
+        out = await asyncio.to_thread(self._codec.decode, chunk_bytes)
         return chunk_spec.prototype.nd_buffer.from_ndarray_like(out.reshape(chunk_spec.shape))
 
     async def _encode_single(self, chunk_ndbuffer: NDBuffer, chunk_spec: ArraySpec) -> Buffer:
         chunk_ndarray = chunk_ndbuffer.as_ndarray_like()
-        out = await to_thread(self._codec.encode, chunk_ndarray)
+        out = await asyncio.to_thread(self._codec.encode, chunk_ndarray)
         return chunk_spec.prototype.buffer.from_bytes(out)
 
 
