4a5a0fdd98c96f9a2ced1a8a1d2d4b9cd98a2537
Cas Wognum <cas.wognum@recursionpharma.com>
2025-01-14 17:26:35 -0500

    Add the UnknownCodecError

---
+++ numcodecs/errors.py
@@ -0,0 +1,17 @@
+"""
+This module defines custom exceptions that are raised in the `numcodecs` codebase.
+"""
+
+class UnknownCodecError(Exception):
+    """
+    An exception that is raised when trying to receive a codec that has not been registered.
+    
+    Parameters
+    ----------
+    codec_id : str
+        Codec identifier.
+    """
+
+    def __init__(self, codec_id: str):
+        self.codec_id = codec_id
+        super().__init__(f"codec not available: '{codec_id}'")
