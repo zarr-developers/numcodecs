16cc9b3a805cb688280e962f1549f2f33b4c46c0
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2025-07-03 16:34:27 -0400

    Test Zstd against pyzstd

---
+++ numcodecs/tests/test_pyzstd.py
@@ -0,0 +1,50 @@
+import pytest
+import numpy as np
+from typing import TYPE_CHECKING
+
+try:
+    from numcodecs.zstd import Zstd
+except ImportError:  # pragma: no cover
+    pytest.skip("numcodecs.zstd not available", allow_module_level=True)
+
+if TYPE_CHECKING:  # pragma: no cover
+    import pyzstd
+else:
+    pyzstd = pytest.importorskip("pyzstd")
+
+test_data = [
+        b"Hello World!",
+        np.arange(113).tobytes(),
+        np.arange(10,15).tobytes(),
+        np.random.randint(3, 50, size=(53,)).tobytes()
+]
+
+@pytest.mark.parametrize("input", test_data)
+def test_pyzstd_simple(input):
+    z = Zstd()
+    assert z.decode(pyzstd.compress(input)) == input
+    assert pyzstd.decompress(z.encode(input)) == input
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("input", test_data)
+def test_pyzstd_simple_multiple_frames(input):
+    z = Zstd()
+    assert z.decode(pyzstd.compress(input)*2) == input*2
+    assert pyzstd.decompress(z.encode(input)*2) == input*2
+
+@pytest.mark.parametrize("input", test_data)
+def test_pyzstd_streaming(input):
+    pyzstd_c = pyzstd.ZstdCompressor()
+    pyzstd_d = pyzstd.ZstdDecompressor()
+    z = Zstd()
+    
+    d_bytes = input
+    pyzstd_c.compress(d_bytes)
+    c_bytes = pyzstd_c.flush()
+    assert z.decode(c_bytes) == d_bytes
+    assert pyzstd_d.decompress(z.encode(d_bytes)) == d_bytes
+
+    # Test multiple streaming frames
+    assert z.decode(c_bytes*2) == d_bytes*2
+    assert z.decode(c_bytes*3) == d_bytes*3
+    assert z.decode(c_bytes*99) == d_bytes*99
