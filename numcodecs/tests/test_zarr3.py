6de860516d0a582c51e1503e8cdb2eb189824686
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-09-23 19:16:52 +0200

    warn when importing from numcodecs.zarr3

--- numcodecs/tests/test_zarr3.py
+++ numcodecs/tests/test_zarr3.py
@@ -8,6 +8,7 @@ if TYPE_CHECKING:  # pragma: no cover
     import zarr
 else:
     zarr = pytest.importorskip("zarr", "3.1.3")
+
 import numcodecs.zarr3 as zarr3
 
 codec_names = [
@@ -40,4 +41,8 @@ def test_export(codec_name: str) -> None:
     """
     Ensure that numcodecs.zarr3 re-exports codecs defined in zarr.codecs.numcodecs
     """
-    assert getattr(zarr3, codec_name) == getattr(zarr.codecs.numcodecs, codec_name)
+    with pytest.warns(
+        DeprecationWarning,
+        match="The numcodecs.zarr3 module is deprecated and will be removed in a future release of numcodecs. ",
+    ):
+        assert getattr(zarr3, codec_name) == getattr(zarr.codecs.numcodecs, codec_name)
